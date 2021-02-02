2757ae86d6f24735451b2012e2833791bb3195ee
Papr1ka <55957109+Papr1ka@users.noreply.github.com>
2021-02-02 20:51:33 +0300

    Update Socket.py

--- Socket.py
+++ Socket.py
@@ -12,7 +12,7 @@ class Socket():
         self.packages = 16192
         self.mainloop = asyncio.get_event_loop()
         self.last_ip = "0.0.0.0" #socket.gethostbyname(socket.gethostname())
-        self.last_port = 80#int(os.environ.get("PORT"))
+        self.last_port = int(os.environ.get("PORT"))
     
     async def send_data(self, data):
         raise NotImplementedError()
